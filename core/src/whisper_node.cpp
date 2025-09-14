4398c2b12b9d5c043852f6c9b9a446982daae508
Saidhury <saidulchoudhury10@gmail.com>
2025-09-14 16:30:11 +0530

    feat(core): Implement UDP sender and broadcast

--- core/src/whisper_node.cpp
+++ core/src/whisper_node.cpp
@@ -11,6 +11,7 @@
     typedef SSIZE_T ssize_t;
     typedef SOCKET socket_t;
     #define CLOSESOCKET(s) closesocket(s)
+    #define SETSOCKOPT_CAST (const char*)
     #pragma comment(lib, "ws2_32.lib")
 #else
     #include <sys/socket.h>
@@ -19,6 +20,7 @@
     #include <unistd.h>
     typedef int socket_t;
     #define CLOSESOCKET(s) close(s)
+    #define SETSOCKOPT_CAST
 #endif
 
 #define BUFFER_SIZE 1024
@@ -77,6 +79,38 @@ void listen_thread_func(int port) {
     CLOSESOCKET(sockfd);
 }
 
+void send_udp_message_internal(const char* message, const char* recipient_ip, int port, bool is_broadcast) {
+    socket_t sockfd;
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("send_udp: socket creation failed");
+        return;
+    }
+
+    if (is_broadcast) {
+        int broadcast_enable = 1;
+        if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST,
+                       SETSOCKOPT_CAST &broadcast_enable, sizeof(broadcast_enable)) < 0) {
+            perror("send_udp: setsockopt(SO_BROADCAST) failed");
+            CLOSESOCKET(sockfd);
+            return;
+        }
+    }
+
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);
+    inet_pton(AF_INET, recipient_ip, &servaddr.sin_addr);
+
+    ssize_t bytes_sent = sendto(sockfd, message, (int)strlen(message), 0,
+                                (const struct sockaddr *)&servaddr, sizeof(servaddr));
+    if (bytes_sent < 0) {
+        perror("send_udp: sendto failed");
+    }
+
+    CLOSESOCKET(sockfd);
+}
+
 extern "C" {
     void start_udp_listener(int port, void (*on_message_received)(const char* message, const char* sender_ip)) {
         initialize_networking();
@@ -85,6 +119,9 @@ extern "C" {
     }
 
     void send_udp_message(const char* message, const char* recipient_ip, int port) {
-        // Placeholder
+        initialize_networking();
+        std::string ip_str(recipient_ip);
+        bool is_broadcast = ip_str.length() > 4 && ip_str.substr(ip_str.length() - 4) == ".255";
+        send_udp_message_internal(message, recipient_ip, port, is_broadcast);
     }
 }
